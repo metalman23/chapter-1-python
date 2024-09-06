c4d4065c91219b23b942ec7f0ab063f5b304092e
metalman23 <20.charlie.gillan@msltcastlegregory.ie>
2024-09-06 17:44:17 +0100

    Create homework3rdsep.py

---
+++ homework3rdsep.py
@@ -0,0 +1,16 @@
+#task 2 pg 18
+a=9.2
+b=6.32
+print("sum =",a*b)
+print ("difference =",a-b)
+print ("product =",a+b)
+
+print ("\n")
+
+#task 5 pg 19
+m=1.60935
+print ("distance to singapore is 7,146 so in km its =",m*7146)
+p=m*7146
+f=900
+print ("fuel is €900 so it will cost =",p*f)
+

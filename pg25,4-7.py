45ceab8a4f52e241ae29088e21d1f26dc0a9ba56
metalman23 <20.charlie.gillan@msltcastlegregory.ie>
2024-09-12 11:03:11 +0100

    Create pg25,4-7.py

---
+++ pg25,4-7.py
@@ -0,0 +1,40 @@
+#pg 25 exercize 4
+# answer = input("please enter a 5 digit number here:")
+# t = answer
+# m = int(t)
+# f = (m/60)
+# a = round(f)
+# print ("the number you gave in seconds is this many minutes:", a)
+# print ("\n")
+
+#exercize 5
+# print ("please enter 5 digits below seperately:")
+# a = input("1:")
+# b = input("2:")
+# c = input("3:")
+# d = input("4:")
+# e = input("5:")
+# print (ord(a))
+# print (ord(b))
+# print (ord(c))
+# print (ord(d))
+# print (ord(e))
+
+#question 6
+# a = input("how many units of energy?:")
+# u = int(a)
+# c = (u*0.19)
+# rc = (c+26.2)
+# print ("that is", rc, "euro every month")
+
+#question 7
+fish = input("please enter how many portions of fish you would like:")
+chips = input("please enter how many portions of chips you would like:")
+of = int(fish)
+oc = int(chips)
+cof = (of*4.5)
+coc = (oc*2.8)
+#how do i get it to 2 decimal places
+rpf = (cof,2)
+rpc = (coc,2)
+print ("it is €", rpf, "for the fish and €,", rpc, "for the chips") 
